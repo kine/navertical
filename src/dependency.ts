7e12b2277fbfeabdda456af12a97006d5e7736eb
miroslav slouka <mslouka@navertica.com>
2021-03-04 15:19:59 +0100

    multi folder dependencies, simple info messages

--- src/dependency.ts
+++ src/dependency.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
-import { readdirSync } from 'fs';
+import { fstat, readdirSync, existsSync } from 'fs';
 import { major, gte } from 'semver';
 import { workspace, window } from 'vscode';
 
 import { GetAzureDevOpsUsername, GetDevOpsPAT, GetAzureCompanyName, GetAzureArtifactFeedID } from './settings';
 
-const getArtifacts = async(url) => {
+const getArtifacts = async (url) => {
     axios.defaults.baseURL = `https://feeds.dev.azure.com/${GetAzureCompanyName()}/`;
 
     const authString = 'Basic' + Buffer.from(GetAzureDevOpsUsername() + ':' + GetDevOpsPAT()).toString('base64');
@@ -24,37 +24,43 @@ const getArtifacts = async(url) => {
     return result.data.value;
 };
 
-const compareToLocalPackages = async(localPath: string) => {
+const createPackageObject = (fileName: string) => {
+    const rx = /(\d+\.)(\d+\.)(\d)/g;
+
+    let packageName = fileName;
+    packageName = packageName.replace('.app', '');
+    packageName = packageName.replace(/(\d+\.)(\d+\.)(\d+\.)(\d)/g, '');
+    packageName = packageName.replace(/ /g, '_');
+    packageName = packageName.slice(0, packageName.length - 1);
+
+    let version = fileName;
+    version = rx.exec(version)[0];
+
+    return {
+        local: {
+            name: packageName,
+            version: version
+        },
+        remote: {}
+    };
+};
+
+const compareToLocalPackages = async (localPath: string) => {
+
+    if (!existsSync(localPath)) {
+        return [];
+    }
+
     const artifacts = await getArtifacts(`_apis/packaging/feeds/${GetAzureArtifactFeedID()}/Packages?api-version=5.0-preview.1`);
 
-    let fileNames = readdirSync(localPath + '/.alpackages');
+    let fileNames = readdirSync(localPath);
 
     fileNames = fileNames.filter(file => !file.toLowerCase().includes('microsoft'));
     const packages = [];
     const outdatedPackages = [];
 
-    const rx = /(\d+\.)(\d+\.)(\d)/g;
     for (let i = 0; i < fileNames.length; i++) {
-        const file = fileNames[i];
-
-        let packageName = file;
-        packageName = packageName.replace('.app', '');
-        packageName = packageName.replace(/(\d+\.)(\d+\.)(\d+\.)(\d)/g, '');
-        packageName = packageName.replace(/ /g, '_');
-        packageName = packageName.slice(0, packageName.length - 1);
-
-        let version = file;
-        version = rx.exec(version)[0];
-
-        fileNames[i] = file;
-
-        packages.push({
-            local: {
-                name: packageName,
-                version: version
-            },
-            remote: {}
-        });
+        packages.push(createPackageObject(fileNames[i]));
     }
 
     artifacts.forEach(a => {
@@ -62,7 +68,7 @@ const compareToLocalPackages = async(localPath: string) => {
 
         if (index !== -1) {
             const localMajorVersion = major(packages[index].local.version);
-            
+
             a.versions.forEach(v => {
                 const remoteMajorVersion = major(v.version);
 
@@ -78,36 +84,61 @@ const compareToLocalPackages = async(localPath: string) => {
 
     packages.forEach(p => {
         const { local, remote } = p;
-        
-        if (!remote.hasOwnProperty('versions')) {
+
+        if (!remote.hasOwnProperty('matchedVersion')) {
             return;
         }
 
         if (gte(remote.matchedVersion, local.version)) {
-            outdatedPackages.push(local);
+            const outdatedPackage = {
+                ...local,
+                newVersion: remote.matchedVersion
+            };
+            outdatedPackages.push(outdatedPackage);
         }
     });
 
     return outdatedPackages;
 };
 
+const getWorkspaceFolderNames = () => {
+    const { workspaceFolders } = workspace;
+    const folders = [];
+
+    for (let i = 0; i < workspaceFolders.length; i++) {
+        const path = workspaceFolders[i].uri.fsPath.split('\\');
+        const folderName = path[path.length - 1];
+        const packageUri = `${workspaceFolders[i].uri.fsPath}\\.alpackages`;
+
+        folders.push({
+            name: folderName,
+            path: packageUri
+        });
+    }
+
+    return folders;
+};
+
+const printOutdatedPackages = (folder) => {
+    compareToLocalPackages(folder.path)
+        .then(outdatedPackages => {
+
+            outdatedPackages.forEach(p => {
+                const message = `${folder.name}: New version for package ${p.name} is available (${p.matchedVersion})`;
+
+                window.showInformationMessage(message);
+            });
+        });
+};
+
 const runCheck = () => {
     // const outdatedPackages = await compareToLocalPackages(workspace.workspaceFolders[0].uri.fsPath);
 
-    compareToLocalPackages(workspace.workspaceFolders[0].uri.fsPath)
-        .then(outdatedPackages => {
-            let message = 'Dependencies:\n';
+    const folders = getWorkspaceFolderNames();
 
-            outdatedPackages.forEach(p => {
-                message += `${p.name}\n`;
-            });
-
-            message += 'have update available';
-
-            window.showErrorMessage(message);
-        });
-
-    // window.showInformationMessage('test', ...outdatedPackages);
+    folders.forEach(f => {
+        printOutdatedPackages(f);
+    });
 };
 
 export { runCheck as default };
